099daa11d8dfcace61c8c2553a96752be8f6c715
Vicky@Developer <91041044+mass9597@users.noreply.github.com>
2025-07-20 19:53:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const inputs = document.querySelectorAll(".code");
 
 inputs.forEach((input,index) =>{
 	input.addEventListener("input",() =>{
-		if(input.value !=="" && index < inputs.length-1){
-			inputs[index+1].focus();
-		}
+		setTimeout(() =>{
+			if(input.value !=="" && index < inputs.length-1){
+			   inputs[index+1].focus();
+		    }
+		},0);
+		
 	});
 
 	input.addEventListener("keydown", (e) =>{
